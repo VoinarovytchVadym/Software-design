691f4a0326406092a2317c87c410ac771202eac4
VoinarovytchVadym <ipz224_vvv@student.ztu.edu.ua>
2024-04-01 15:13:04 +0300

    Внесені зміни до коду

--- Lab_1/Lab_1/Program.cs
+++ Lab_1/Lab_1/Program.cs
@@ -8,14 +8,12 @@ class Program
         Console.OutputEncoding = Encoding.UTF8;
 
         Warehouse[] warehouses = new Warehouse[4];
-        Warehouse warehouse1 = new Warehouse(new Product("Product-1", new USD(12, 50)), 1000, "шт.", DateTime.Now);
-        Warehouse warehouse2 = new Warehouse(new Product("Product-2", new USD(121, 0)), 5, "кг.", DateTime.Now);
-        Warehouse warehouse3 = new Warehouse(new Product("Product-3", new USD(1782, 20)), 3, "шт.", DateTime.Now);
-        Warehouse warehouse4 = new Warehouse(new Product("Product-4", new USD(1, 50)), 100, "л.", DateTime.Now);
-        warehouses[0]= warehouse1;
-        warehouses[1]= warehouse2;
-        warehouses[2]= warehouse3;
-        warehouses[3]= warehouse4;
+        Product product = new Product("Product-1", new USD(12, 50));
+        product.Discount(new USD(1, 50));
+        warehouses[0]= new Warehouse(product, 1000, "шт.", DateTime.Now);
+        warehouses[1]= new Warehouse(new Product("Product-2", new USD(121, 0)), 5, "кг.", DateTime.Now);
+        warehouses[2]= new Warehouse(new Product("Product-3", new USD(1782, 20)), 3, "шт.", DateTime.Now);
+        warehouses[3]= new Warehouse(new Product("Product-4", new USD(1, 50)), 100, "л.", DateTime.Now);
         RevenueInvoice revenueInvoice = new RevenueInvoice(warehouses);
         SalesInvoice salesInvoice = new SalesInvoice(warehouses, "Post", "Costumer", "Contract");
         InventoryReport inventoryReport = new InventoryReport(warehouses);
