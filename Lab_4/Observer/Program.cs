d09fcf0a4b7dd41b0f5c190ac82f4f25f43175f8
VoinarovytchVadym <ipz224_vvv@student.ztu.edu.ua>
2024-05-26 14:01:58 +0300

    Lab_4 repaired

---
+++ Lab_4/Observer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Composer.Nodes;
+
+namespace Composer;
+
+public abstract class Program
+{
+    public static void Main()
+    {
+        var button = new LightElementNode("button", "inline", "closing",
+            ["button"],
+            [new LightTextNode("Submit")]);
+
+        button.AddEventListener("click", () => Console.WriteLine("Button clicked!"));
+        button.AddEventListener("mouseover", () => Console.WriteLine("Mouse is over button!"));
+        button.TriggerEvent("click");
+        button.TriggerEvent("mouseover");
+    }
+}
